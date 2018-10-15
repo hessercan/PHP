8ccefeb610007fa9b6b2d135a049dc94fec4056d
hessercan <hessercan@icloud.com>
2018-10-15 11:50:30 -0400

    15

--- includes/navbar.php
+++ includes/navbar.php
@@ -42,28 +42,40 @@
         }
 
         if( isset($_SESSION['username']) ) {
+
+          //Upload
           echo "<td class=\"navbar\">";
           echo (basename($_SERVER['PHP_SELF']) == "upload.php")
           ? "<a href=\"upload.php\"><strong>Upload</strong></a>"
           : "<a href=\"upload.php\">Upload</a>";
           echo "</td>";
 
+          //Users
           echo "<td class=\"navbar\">";
           echo (basename($_SERVER['PHP_SELF']) == "users.php")
           ? "<a href=\"users.php\"><strong>Users</strong></a>"
           : "<a href=\"users.php\">Users</a>";
           echo "</td>";
 
+          //Cookies
           echo "<td class=\"navbar\">";
           echo (basename($_SERVER['PHP_SELF']) == "cookies.php")
           ? "<a href=\"cookies.php\"><strong>Cookies</strong></a>"
           : "<a href=\"cookies.php\">Cookies</a>";
           echo "</td>";
 
+          //Shell Exec
           echo "<td class=\"navbar\">";
           echo (basename($_SERVER['PHP_SELF']) == "shell_exec.php")
           ? "<a href=\"shell_exec.php\"><strong>Shell</strong></a>"
           : "<a href=\"shell_exec.php\">Shell</a>";
+          echo "</td>";
+
+          //FollowMe
+          echo "<td class=\"navbar\">";
+          echo (basename($_SERVER['PHP_SELF']) == "followme/index.html")
+          ? "<a href=\"followme/index.html\"><strong>Follow Me</strong></a>"
+          : "<a href=\"followme/index.html\">Follow Me</a>";
           echo "</td>";
         }?>
       </tr>
